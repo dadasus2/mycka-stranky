e590d532335f5c826aa3b72a28e4537f67af2be8
Daniel <bohatec.daniel@stud.vassboskovice.cz>
2023-08-01 15:19:50 +0200

    cursor

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ closeMenu.addEventListener('click', () => {
     header.classList.remove('ztmav')
 })
 
+var cursor = document.querySelector('.cursor')
+var cursor2 = document.querySelector('.cursor2')
+document.addEventListener('mousemove', function (e) {
+    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+})
+
